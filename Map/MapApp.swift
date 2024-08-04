cdfee9a50df63f0f335dba4190d39948dd130af9
Nadi Furkan Açıkgöz <n.furkan.acikgoz@gmail.com>
2024-08-04 12:51:10 +0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Furkan Açıkgöz on 4.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
